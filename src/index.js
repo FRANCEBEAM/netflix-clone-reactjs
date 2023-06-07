6efd9834353296d2ab17aef5ba8941a32547ddd1
FRANCEBEAM <santanderfrancisbeam@gmail.com>
2023-06-07 09:02:06 +0800

    added 404 debugging pages

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link }from "react-router-dom";
+
+
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -14,35 +12,25 @@ import Account from "./pages/Account";
 
 // import { AuthContextProvider } from './context/AuthContext';
 
+const AppLayout = () => {
+
+  return(
+    <>
+    <App />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </>
+  )
+}
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: [<App />, <Outlet />],
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "signin",
-        element: <SignIn />,
-      },
-      {
-        path: "signup",
-        element: <SignUp />,
-      },
-      {
-        path: "account",
-        element: <Account />,
-      },
-    ],
-  },
-]);
-
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
+ReactDOM.render(
+  <Router basename='/'>
+    <AppLayout />
+  </Router>,
+  document.getElementById('root')
+)
